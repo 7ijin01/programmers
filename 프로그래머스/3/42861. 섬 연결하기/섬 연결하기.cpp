191c95909c5ab115184371ba5f3f4c0633861f3a
웃기진않아 <63440088+7ijin01@users.noreply.github.com>
2025-06-13 09:51:25 +0900

    [level 3] Title: 섬 연결하기, Time: 0.10 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -2,15 +2,13 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+int answer=0;
+vector<int> parents;
 
-bool compare(vector<int> a,vector<int> b)
+bool compare(vector<int> a,vector<int>b)
 {
-    return a[2] < b[2];
+    return a[2]<b[2];
 }
-vector<int>parents;
-
-
-
 
 int find(int x)
 {
@@ -20,28 +18,24 @@ int find(int x)
     return parents[x];
 }
 
-int answer = 0;
 bool unionSet(int x,int y)
 {
-    int node1=find(x);
-    int node2=find(y);
-    if(node1!=node2)
+    int a=find(x);
+    int b=find(y);
+    if(a!=b)
     {
-        parents[node2] = node1;
+        parents[b]=parents[a];
         return true;
     }
     return false;
-    
 }
 
-
 int solution(int n, vector<vector<int>> costs) {
-    
     sort(costs.begin(),costs.end(),compare);
     parents.resize(n);
-    for(int i = 0; i < n; i++)
+    for(int i=0;i<n;i++)
     {
-        parents[i] = i;
+        parents[i]=i;
     }
     for(vector<int> cost:costs)
     {
@@ -50,6 +44,8 @@ int solution(int n, vector<vector<int>> costs) {
             answer+=cost[2];
         }
     }
+    
+    
     
     
     return answer;
